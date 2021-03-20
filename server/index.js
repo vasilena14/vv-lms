0279ae4ecfabd7b6e02926bcbe1c012900df725a
vasilena14 <vassilena.v14@gmail.com>
2021-03-20 22:07:25 +0200

    Downloaded dependencies and switched back to localhost URLs

--- server/index.js
+++ server/index.js
@@ -15,13 +15,15 @@ app.use(cors());
 
 app.use('/courses', courseRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello to VV-LMS API');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello to VV-LMS API');
+// });
 
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = "mongodb+srv://user123:123user123@cluster0.6umk0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
