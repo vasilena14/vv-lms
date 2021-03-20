619306dad1690ec22f35264b7641fce9d544466a
vasilena14 <vassilena.v14@gmail.com>
2021-03-20 21:54:31 +0200

    Changed localhost api url to heroku url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/courses';
+const url = 'https://vv-lms.herokuapp.com/courses';
 
 export const fetchCourses = () => axios.get(url);
 export const createCourse = (newCourse) => axios.post(url, newCourse);
