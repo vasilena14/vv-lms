a8ac231f7be111a02a8822cb6b860d1ac3ead104
vasilena14 <vassilena.v14@gmail.com>
2021-03-27 10:57:18 +0200

    Created Layout for authorization

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Auth() {
+    const state = null;
+    return (
+        <div>
+            AUTH
+        </div>
+    )
+}
+
+export default Auth
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import {Container, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getCourses } from '../../actions/courses';
+
+import Courses from '../Courses/Courses';
+import Form from '../Form/Form';
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(null);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCourses());
+    }, [currentId, dispatch]);
+
+    return (
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item sx={12} sm={7}>
+                        <Courses setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item sx={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    )
+}
+
+export default Home;
